20e811f9c87cc08d143119c113df68aba2554cac
AdamAmon <1105607453@qq.com>
2025-08-02 19:57:50 +0800

    security: 尝试修复koa的两个低风险漏洞，但修复会导致4个高风险漏洞，回退版本

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,5 +1,3 @@
 // 自动扫描 src/controller 目录，确保 jest 测试时 controller 路由可用
-const { join } = require('path');
-module.exports = {
-  imports: [join(__dirname, '../src/controller')],
-}; 
+import { join } from 'path';
+export const imports = [join(__dirname, '../src/controller')]; 
